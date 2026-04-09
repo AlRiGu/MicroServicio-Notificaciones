7dd364495eb692b3b4980da64e09d7b7636d711a
AlRiGu <lasierra2005@gmail.com>
2026-04-09 13:11:15 +0200

    Cambiado el pom.xml y añadido el NotiAplicacion

---
+++ src/main/java/NotiAplicacion.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotiAplicacion {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotiAplicacion.class, args);
+	}
+
+}
